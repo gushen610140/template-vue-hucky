15885463d34ddd021067d8201ccc16fee23c0fc9
gushen610140 <w56038009@outlook.com>
2025-09-15 09:00:41 +0800

    feat: add auto import

--- src/pages/HelpPage.vue
+++ src/pages/HelpPage.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
 import { range } from "radash";
 import { usePagination } from "@/utils/pagination";
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueJsx()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vueJsx(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
